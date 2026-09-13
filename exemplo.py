77576fcf654ab3c6cc2a30d699501cbce657372f
ATLAS <sr.meianoite15@gmail.com>
2026-09-13 20:26:05 -0300

    código atualizado

---
+++ exemplo.py
@@ -0,0 +1,6 @@
+if opcao == "1":
+    quantidade_livros = input("Quantos livros deseja cadastrar? ")
+
+    for i in range(quantidade_livros):
+        print("LIVRO")
+        codigo = input("codigo: ")
